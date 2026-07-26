476a7a63a7076166d40681a054cfd076187d7b25
llipinsk82-rgb <llipinsk82@gmail.com>
2026-07-26 23:31:30 +0100

    build: add extended icon set for custom UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,6 +105,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.material:material-icons-extended")
     implementation(libs.androidx.compose.ui.tooling.preview)
     debugImplementation(libs.androidx.compose.ui.tooling)
 
